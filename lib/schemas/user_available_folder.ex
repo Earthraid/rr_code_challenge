486dac144acb56a399dfc714a5468ed337630ba8
Jerrad <jerradchristian@gmail.com>
2022-10-11 20:41:48 -0400

    UserAvailableFolder
    Added cast and required fields and update changeset and default attrs to the changeset function

--- lib/schemas/user_available_folder.ex
+++ lib/schemas/user_available_folder.ex
@@ -2,6 +2,8 @@ defmodule Schemas.UserAvailableFolder do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @required_fields [:customer_folder_id, :user_id]
+
   schema "user_available_folders" do
     belongs_to :customer_folder, Schemas.CustomerFolder
     belongs_to :user, Schemas.User
@@ -10,9 +12,15 @@ defmodule Schemas.UserAvailableFolder do
   end
 
   @doc false
-  def changeset(user_available_folder, attrs) do
+  def update_changeset(user_available_folder, attrs \\ %{}) do
     user_available_folder
-    |> cast(attrs, [])
-    |> validate_required([])
+    |> cast(attrs, @required_fields)
+    |> validate_required(@required_fields)
+  end
+
+  def changeset(attrs \\ %{}) do
+    %__MODULE__{}
+    |> cast(attrs, @required_fields)
+    |> validate_required(@required_fields)
   end
 end
